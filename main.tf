25251c9de07d278a6370081926057cfb7a0477a5
Mo Engelbrecht <10662677+lengebre@users.noreply.github.com>
2022-09-24 13:09:13 -0400

    Update AMI to ubuntu. Update User data to change on true. Add security group to allow viewing of website

--- main.tf
+++ main.tf
@@ -1,25 +1,51 @@
-provider "aws"{
-    region = "us-east-1"
+provider "aws" {
+  region = "us-east-1"
 }
 
-data "aws_ami" "amazon-linux-2" {
-  most_recent = true
+data "aws_ami" "ubuntu" {
 
-  filter {
-    name   = "owner-alias"
-    values = ["amazon"]
-  }
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 }
 
 resource "aws_instance" "web" {
-    ami = data.aws_ami.amazon-linux-2.id
-    instance_type = "t2.micro"
-    tags = {
-        Name = "Web Server"
-    }
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
+  tags = {
+    Name = "Web Server"
+  }
+}
+
+resource "aws_security_group" "web" {
+  name        = "web"
+  description = "Allow HTTP traffic"
+
+  ingress {
+    description = "HTTP"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Type = "Web Server"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
 }
